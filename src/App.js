015cae541554cd41311108e4f04350e9b399790a
hnekhniadovich <hanna@nextguest.com>
2020-07-15 22:09:50 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Card Memory Game
+      <div>
+      <i className="fa fa-motorcycle"></i>
+      <i className="fa fa-binoculars"></i>
+      <i className="fa fa-optin-monster"></i>
+      <i className="fa fa-fort-awesome"></i>
+      <i className="fa fa-snowflake-o"></i>
+      <i className="fa fa-wpexplorer"></i>
+      <i className="fa fa-bath"></i>
+      <i className="fa fa-camera-retro"></i>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//https://www.svgbackgrounds.com/
